7d430fe4a841d4b1a1467ee7b6308c7911a17f8c
siloksan <107646198+siloksan@users.noreply.github.com>
2024-03-29 17:01:40 +0300

    Update repeater.js

--- repeater.js
+++ repeater.js
@@ -1,14 +1,14 @@
 String.prototype.repeat = null;
 
 Object.defineProperty(String.prototype, 'repeat', {
-  value: function (count) {
+  value: function (count) {//количество повторений
     if (count < 0 || !Number.isInteger(count)) {
       throw new Error('The argument is non-integer or less than 0');
     }
     if (count === 0) return ''
     let newString = '';
-    while (count) {
-      newString += this;
+    while (count > 0) {
+      newString += this;//в нашу новую строку добавляем исходную
       count -= 1;
     }
     return newString;
