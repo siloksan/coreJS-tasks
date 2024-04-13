0e2e4c760aa95b79cf413e87052236c95e5d8b27
siloksan <107646198+siloksan@users.noreply.github.com>
2024-04-13 09:02:25 +0300

    Update curryLogger.js

--- curryLogger.js
+++ curryLogger.js
@@ -1,8 +1,18 @@
-function curryLogger (outerText) {
-  return function (innerText) {
-    return console.log(outerText,': ', innerText)
-  }
+function curryLogger(logger) {
+  return function(...args) {
+    return function(message) {
+      logger(message, ...args);
+    };
+  };
 }
 
-const curry  = curryLogger('result');
-curry('success')
+// Example usage
+function customLogger(message, level) {
+  console.log(`[${level}] ${message}`);
+}
+
+const curriedCustomLogger = curryLogger(customLogger);
+
+const infoLogger = curriedCustomLogger('INFO');
+infoLogger('This is an information message');
+infoLogger('Another information message');
